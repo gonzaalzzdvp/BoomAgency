1fef1c413fea3af1d90636e797eac751eb02da7e
gonzaalzz <hectorlgonzalez91@gmail.com>
2026-04-17 13:36:01 -0400

    BoomAgency v.1.0.0

--- frontend/src/components/aboutUS/AboutHero.jsx
+++ frontend/src/components/aboutUS/AboutHero.jsx
@@ -1,20 +1,33 @@
 import TeamCard from "./TeamCard";
-import Mafer from "/carousel/Mafer.png";
-import Hector from "/carousel/Hector.png";
-import Amanda from "/carousel/Amanda.png";
-import Glennis from "/carousel/Glennis.png";
-
 
 export default function AboutHero() {
   return (
     <div className="lg:h-screen w-full py-20 flex flex-col justify-center items-center gap-20">
-      <h3 className="text-3xl md:text-4xl font-bold text-(--blueBoom) uppercase">Nuestro equipo</h3>
+      <h3 className="text-3xl md:text-4xl font-bold text-(--blueBoom) uppercase">
+        Nuestro equipo
+      </h3>
       <div className="w-full flex flex-col justify-center items-center md:grid md:grid-cols-2 md:justify-items-center lg:flex lg:flex-row gap-10">
-        <TeamCard img={Mafer} name="Mafer Maldonado" workstation="CEO" />
-        <TeamCard img={Glennis} name="Glennis Ybirma" workstation="CEO" />
-        <TeamCard img={Amanda} name="Amanda Gonzalez" workstation="CEO" />
-        <TeamCard img={Mafer} name="Mafer" workstation="CEO" />
-        <TeamCard img={Hector} name="Hector Gonzalez" workstation="CEO" />
+        <TeamCard
+          img="/carousel/Mafer.png"
+          name="Mafer Maldonado"
+          workstation="CEO"
+        />
+        <TeamCard
+          img="/carousel/Glennis.png"
+          name="Glennis Ybirma"
+          workstation="CEO"
+        />
+        <TeamCard
+          img="/carousel/Amanda.png"
+          name="Amanda Gonzalez"
+          workstation="CEO"
+        />
+        <TeamCard img="/carousel/Mafer.png" name="Mafer" workstation="CEO" />
+        <TeamCard
+          img="/carousel/Hector.png"
+          name="Hector Gonzalez"
+          workstation="CEO"
+        />
       </div>
     </div>
   );
